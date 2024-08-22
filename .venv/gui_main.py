43ab56f35112e34772f4f7dde74f005b4e65895c
stranger <tkpanio@gmail.com>
2024-08-22 14:26:53 +0200

    ''zz

--- .venv/gui_main.py
+++ .venv/gui_main.py
@@ -1,8 +1,12 @@
+from tkinter.ttk import Label
+
 import functione
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
 
-label = FreeSimpleGUI.Text("Type in")
+label = sg.Text("Type in a todo")
+input_box = sg.InputText(tooltip="Enter a todo" )
+add_botton = sg.Button("Add todo")
 
-window = FreeSimpleGUI.Window("My Todo App", layout=[""])
+window = sg.Window("My Todo App", layout=[[label, input_box, add_botton] ])
 window.read()
 window.close()
